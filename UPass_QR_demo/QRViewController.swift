1b9c7eeabeca511817375a7035ff6293ce8cd611
chenyonghuazi <edwinchenyonghua@gmail.com>
2018-01-01 17:11:12 -0500

    first commit

---
+++ UPass_QR_demo/QRViewController.swift
@@ -0,0 +1,39 @@
+//
+//  QRViewController.swift
+//  UPass_QR_demo
+//
+//  Created by Edwin on 2018/1/1.
+//  Copyright © 2018年 Edwin. All rights reserved.
+//
+
+import UIKit
+
+class QRViewController: UIViewController {
+
+    @IBOutlet weak var QRImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UPass_QR_demo/ViewController.swift
+++ UPass_QR_demo/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    var filer: CIFilter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,54 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func createQR(_ sender: UIButton) {
+        if firstNameTextField.text != "" && lastNameTextField.text != ""
+            && emailTextField.text != "" && phoneTextField.text != ""{
+//            var dataDictionary = [String:String]()
+//            dataDictionary["firstName"] = firstNameTextField.text
+//            dataDictionary["lastName"] = lastNameTextField.text
+//            dataDictionary["email"] = emailTextField.text
+//            dataDictionary["phoneNumber"] = phoneTextField.text
+            
+            var textBundle = ""
+            textBundle = textBundle + firstNameTextField.text!
+            textBundle = textBundle + lastNameTextField.text!
+            textBundle = textBundle + emailTextField.text!
+            textBundle = String(textBundle + phoneTextField.text!)
+            let data = textBundle.data(using: .ascii, allowLossyConversion: false)
+            
+            filer = CIFilter(name: "CIQRCodeGenerator")
+            
+            filer?.setValue(data, forKey: "inputMessage")
+            
+            transforSegue()
+    }
+        
+        
+    
+}
+    func transforSegue(){
+        let transform = CGAffineTransform(scaleX: 10, y: 10)
+        let story = UIStoryboard(name: "Main", bundle: nil)
+        let destVC = story.instantiateViewController(withIdentifier: "QRViewController") as! QRViewController
+        if filer == nil{
+            print("Error")
+        }
+        if filer?.outputImage == nil {
+            print("filter.outputImage is nil")
+        }
+        if filer?.outputImage?.transformed(by: transform) == nil{
+            print("you are nil")
+            return
+        }
+        let image = UIImage(ciImage: filer!.outputImage!.transformed(by: transform))
+        destVC.QRImageView.image = image
+        
+        present(destVC, animated: true, completion: nil)
+    }
 
 }
-
